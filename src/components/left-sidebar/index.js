b3fe70b21e59b1a16795e2e32516a608d3809715
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-10 23:38:54 +0600

    menu items added

--- src/components/left-sidebar/index.js
+++ src/components/left-sidebar/index.js
@@ -1,11 +1,70 @@
 import React from "react";
 import logo from "../../assets/images/logo.svg";
 
+// icons
+import dashboardIcon from "../../assets/images/dashboard.svg";
+import inventoryIcon from "../../assets/images/inventory.svg";
+import documentIcon from "../../assets/images/document.svg";
+import profileIcon from "../../assets/images/profile.svg";
+import commentIcon from "../../assets/images/comment.svg";
+import cogIcon from "../../assets/images/cog.svg";
+
+// router
+import { NavLink } from "react-router-dom";
+
 const LeftSidebar = () => {
+  const menuData = [
+    {
+      id: 1,
+      name: "Dashboard",
+      icon: dashboardIcon,
+      link: "/",
+    },
+    {
+      id: 2,
+      name: "Inventory",
+      icon: inventoryIcon,
+      link: "/inventory",
+    },
+    {
+      id: 2,
+      name: "Documents",
+      icon: documentIcon,
+      link: "/docs",
+    },
+    {
+      id: 2,
+      name: "Communication",
+      icon: profileIcon,
+      link: "/communication",
+    },
+    {
+      id: 2,
+      name: "Internal Communication",
+      icon: commentIcon,
+      link: "/internal-communication",
+    },
+    {
+      id: 2,
+      name: "Settings",
+      icon: cogIcon,
+      link: "/settings",
+    },
+  ];
   return (
-    <div>
+    <div className="sidebar">
       <div className="flex items-center justify-center menu-flex">
-        <img src={logo} alt="logo" className="logo" />
+        <div className="w-full">
+          <img src={logo} alt="logo" className="logo" />
+          <div className="menu">
+            {menuData.map((item, index) => (
+              <NavLink to={item.link} key={index} className="menu-item">
+                <img src={item.icon} alt={item.name} className="menu-icon" />
+                <span className="menu-text">{item.name}</span>
+              </NavLink>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
-import Home from "./pages/main";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(<RouterProvider router={router} />);
