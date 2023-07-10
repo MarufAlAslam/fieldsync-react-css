5dc6b8b98671bf417ba1265e7068ea72acb615c6
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-07-11 00:24:23 +0600

    main page done

---
+++ src/components/cards/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// import card1 from "../../assets/images/card1.svg";
+// import card2 from "../../assets/images/card2.svg";
+// import card3 from "../../assets/images/card3.svg";
+// import card4 from "../../assets/images/card4.svg";
+// import card5 from "../../assets/images/card5.svg";
+
+const Cards = () => {
+  return (
+    <div className="grid grid-cols-3 gap-25">
+      <div className="col-span-2 card1 card-h">
+        <p className="fs-24 text-white medium">New Work Order</p>
+        <p className="text-white fs-18">Create a new work order</p>
+      </div>
+      <div className="card2 card-h">
+        <p className="fs-24 text-white medium">Overview</p>
+        <p className="text-white fs-18">Overview of all work orders</p>
+      </div>
+      <div className="card3 card-h">
+        <p className="fs-24 text-white medium">Client list</p>
+        <p className="text-white fs-18">Client list with details</p>
+      </div>
+      <div className="card4 card-h">
+        <p className="fs-24 text-white medium">Calendar</p>
+        <p className="text-white fs-18">Calendar overview</p>
+      </div>
+      <div className="card5 card-h">
+        <p className="fs-24 text-white medium">Billing</p>
+        <p className="text-white fs-18">Overview of invoices</p>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Cards from "../cards";
+
+import bell from "../../assets/images/bell.svg";
 
 const Main = () => {
-  return <div>main js</div>;
+  return (
+    <div className="main-panel">
+      <div className="flex justify-between align-center">
+        <p className="fs-24 medium text-blue mb-30">
+          Monday - 3 July 2023 - 12:15:34
+        </p>
+        <img src={bell} alt="bell" className="bell" />
+      </div>
+      <h2 className="fs-28 text-blue medium">Dashboard</h2>
+
+      <div className="cards-holder">
+        <Cards />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
